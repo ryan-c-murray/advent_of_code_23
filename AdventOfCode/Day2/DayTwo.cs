8e66110ac805889f1e765fee3c88b27ca9d92a7a
Ryan Murray <Ryan@Ryans-Macbook-Pro-2.local>
2023-12-03 01:02:53 -0600

    added sum data

--- AdventOfCode/Day2/DayTwo.cs
+++ AdventOfCode/Day2/DayTwo.cs
@@ -7,14 +7,48 @@ namespace AdventOfCode.Day2
     {      
         public Dictionary<string,List<Dictionary<string,int>>> InputData = new();
 
+        public Dictionary<string,Dictionary<string,int>> SumData = new();
+
 
         public DayTwo()
         {
             DayTwoInputParse inputParser = new DayTwoInputParse();
             InputData = inputParser.GetInput();
 
+            SumData = getSumData(InputData);
+
             Console.WriteLine("done");
         }
 
+        private Dictionary<string,Dictionary<string,int>> getSumData(Dictionary<string,List<Dictionary<string,int>>> m_inputData)
+        {
+            Dictionary<string,Dictionary<string,int>> sumData = new();
+
+            foreach (var gamePair in m_inputData)
+            {
+                sumData[gamePair.Key] = addList(gamePair.Value);
+            }
+            
+            return sumData;
+        }
+
+        private Dictionary<string,int> addList(List<Dictionary<string,int>> roundList)
+        {
+            Dictionary<string,int> roundSum = new()
+            {
+                {"red",0},
+                {"green",0},
+                {"blue",0}
+            };
+            foreach (Dictionary<string,int> round in roundList)
+            {
+                foreach (var colorDictPair in round)
+                {
+                    roundSum[colorDictPair.Key] += colorDictPair.Value;
+                }
+            }
+
+            return roundSum;
+        }
     }
 }
